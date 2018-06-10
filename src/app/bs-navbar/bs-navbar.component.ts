2119575826e650fc439048cf650c1aeb87bafabb
Shubhmeet Kaur <sxk162731@utdallas.edu>
2018-06-09 20:14:16 -0500

    Admin authorisation added

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {AngularFireAuth} from 'angularfire2/auth';
+import {Observable} from 'rxjs/Observable';
+import {AuthService} from '../auth.service';
+import {AppUser} from '../models/app-user';
 
 @Component({
   selector: 'app-bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
 
-  constructor() { }
+  appUser: AppUser;
 
-  ngOnInit() {
+  constructor(public auth: AuthService) {
+    auth.appUser$.subscribe( appUser => this.appUser = appUser);
   }
 
+  logout() {
+    this.auth.logout();
+  }
 }
